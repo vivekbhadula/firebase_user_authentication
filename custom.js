cea92e391b3bdf1c3f52531bdf34ca84f620aeb6
Jurgen <efa.jkz@gmail.com>
2024-02-16 18:40:24 +0100

    Implement logical answer and handle check answer

--- custom.js
+++ custom.js
@@ -3,7 +3,9 @@ window.globalQIndex = globalQIndex;
 
 let questionIndex = globalQIndex + 1;
 
-const submitModal = new bootstrap.Modal(document.getElementById('submitModal'), {})
+const submitModal = new bootstrap.Modal(document.getElementById('submitModal'), {});
+const toast = new bootstrap.Toast(document.getElementById('liveToast'), {});
+let answersSubmitIndex = [];
 
 function showQuestion(index) {
     let questions = document.querySelectorAll(".question");
@@ -15,13 +17,30 @@ function showQuestion(index) {
     });
 };
 
+function submitAnswer(index) {
+    let question = Array.from(document.querySelectorAll(".question")).filter( (q, i) => i === index)[0];
+    let answers = question.querySelectorAll('.answer-box input[type="radio"]');
+    let checkIfAnswered = Array.from(answers).filter( (a, i) => a.checked === true);
+    if (checkIfAnswered.length == 0) {
+        toast.show();
+        return -1;
+    }
+    answers.forEach((answer, answerIndex) => {
+        if (answer.checked) {
+            answersSubmitIndex.push(answerIndex + 1);
+        }
+    });
+};
+
 function incrementIndex(){
     if (globalQIndex + 1 > 4) {
         globalQIndex = 0;
     } else {
-        submitModal.hide();
-        globalQIndex = globalQIndex + 1;        
+        let submitResult = submitAnswer(globalQIndex);
+        if (submitResult == -1) return;
+        globalQIndex = globalQIndex + 1;       
     }
+    submitModal.hide();
     showQuestion(globalQIndex)
 }
 
